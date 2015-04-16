127f3eeb81e31921153209e4be49307b0f014582
Shipeng Feng <fsp261@gmail.com>
2015-04-17 00:51:06 +0800

    Added more

--- colorthief.py
+++ colorthief.py
@@ -106,6 +106,84 @@ class MMCQ(object):
         return VBox(rmin, rmax, gmin, gmax, bmin, bmax, histo)
 
     @staticmethod
+    def median_cut_apply(histo, vbox):
+        if not vbox.count:
+            return None
+
+        rw = vbox.r2 - vbox.r1 +1
+        gw = vbox.g2 - vbox.g1 +1
+        bw = vbox.b2 - vbox.b1 +1
+        maxw = max([rw, gw, bw])
+        # only one pixel, no split
+        if vbox.count == 1:
+            return (vbox.copy,)
+        # Find the partial sum arrays along the selected axis.
+        total = 0
+        sum_ = 0
+        partialsum = {}
+        lookaheadsum = {}
+        do_cut_color = None
+        if maxw == rw:
+            do_cut_color = 'r'
+            for i in range(vbox.r1, vbox.r2+1):
+                sum_ = 0
+                for j in range(vbox.g1, vbox.g2+1):
+                    for k in range(vbox.b1, vbox.b2+1):
+                        index = MMCQ.get_color_index(i, j, k)
+                        sum_ += histo.get(index, 0)
+                total += sum_
+                partialsum[i] = total
+        elif maxw == gw:
+            do_cut_color = 'g'
+            for i in range(vbox.g1, vbox.g2+1):
+                sum_ = 0
+                for j in range(vbox.r1, vbox.r2+1):
+                    for k in range(vbox.b1, vbox.b2+1):
+                        index = MMCQ.get_color_index(j, i, k)
+                        sum_ += histo.get(index, 0)
+                total += sum_
+                partialsum[i] = total
+        else:  # maxw == bw
+            do_cut_color = 'b'
+            for i in range(vbox.b1, vbox.b2+1):
+                sum_ = 0
+                for j in range(vbox.r1, vbox.r2+1):
+                    for k in range(vbox.g1, vbox.g2+1):
+                        index = MMCQ.get_color_index(j, k, i)
+                        sum_ += histo.get(index, 0)
+                total += sum_
+                partialsum[i] = total
+        for i, d in partialsum.iteritems():
+            lookaheadsum[i] = total - d
+
+        # determine the cut planes
+        dim1 = do_cut_color + '1'
+        dim2 = do_cut_color + '2'
+        dim1_val = getattr(vbox, dim1)
+        dim2_val = getattr(vbox, dim2)
+        for i in range(dim1_val, dim2_val+1):
+            if partialsum[i] > (total / 2):
+                vbox1 = vbox.copy
+                vbox2 = vbox.copy
+                left = i - dim1_val
+                right = dim2_val - i
+                if left <= right:
+                    d2 = min([dim2_val - 1, int(i + right / 2)])
+                else:
+                    d2 = max([dim1_val, int(i - 1 - left / 2)])
+                # avoid 0-count boxes
+                while not partialsum[d2]:
+                    d2 += 1
+                count2 = lookaheadsum[d2]
+                while not count2 and partialsum.get(d2-1, False):
+                    d2 -= 1
+                    count2 = lookaheadsum[d2]
+                # set dimensions
+                setattr(vbox1, dim2, d2)
+                setattr(vbox2, dim1, getattr(vbox1, dim2) + 1)
+                return (vbox1, vbox2)
+
+    @staticmethod
     def quantize(pixels, max_color):
         """Quantize.
 
