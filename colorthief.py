dee90ccf5ca91001cc79316e6ea93aaa9fe1bff9
Shipeng Feng <fsp261@gmail.com>
2015-04-16 23:54:36 +0800

    Added more

--- colorthief.py
+++ colorthief.py
@@ -8,10 +8,275 @@
     :copyright: (c) 2015 by Shipeng Feng.
     :license: BSD, see LICENSE for more details.
 """
-
 __version__ = '0.1-dev'
+
+import math
+
+
+class cached_property(object):
+    """Decorator that creates converts a method with a single
+    self argument into a property cached on the instance.
+    """
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, type):
+        res = instance.__dict__[self.func.__name__] = self.func(instance)
+        return res
 
 
 class ColorThief(object):
     """Color thief main class."""
-    pass
+    def __init__(self):
+        pass
+
+    def get_color(self, quality=10):
+        """Get the dominant color.
+
+        Quality is an optional argument.  0 is the highest quality settings,
+        10 is the default.  The bigger the number, the faster a color will be
+        returned but the greater the likelihood that is will not be the most
+        dominant color.
+
+        :param quality: needs to be an integer
+        :return tuple: (r, g, b)
+        """
+        palette = self.get_palette(5, quality)
+        return palette[0]
+
+    def get_palette(self, count=10, quality=10):
+        """Build a color palette.  We are using the median cut algorithm to
+        cluster similar colors.
+        
+        :param count: the size of the palette, max number of colors returned
+        :param quality: quality settings, 0 is the highest quality, the bigger
+                        the number, the faster the palette generation
+        :return list: a list of tuple in the form (r, g, b)
+        """
+        pass
+
+
+class MMCQ(object):
+    """Basic Python port of the MMCQ (modified median cut quantization)
+    algorithm from the Leptonica library (http://www.leptonica.com/).
+    """
+
+    SIGBITS = 5
+    RSHIFT = 8 - SIGBITS
+    MAX_ITERATION = 1000
+    FRACT_BY_POPULATIONS = 0.75
+
+    @staticmethod
+    def get_color_index(r, g, b):
+        return (r << (2 * MMCQ.SIGBITS)) + (g << MMCQ.SIGBITS) + b
+
+    @staticmethod
+    def get_histo(pixels):
+        """histo (1-d array, giving the number of pixels in each quantized
+        region of color space)
+        """
+        histo_size = 1 << (3 * MMCQ.SIGBITS)
+        histo = dict()
+        for pixel in pixels:
+            rval = pixel[0] >> MMCQ.RSHIFT
+            gval = pixel[1] >> MMCQ.RSHIFT
+            bval = pixel[2] >> MMCQ.RSHIFT
+            index = MMCQ.get_color_index(rval, gval, bval)
+            histo[index] = histo.setdefault(index, 0) + 1
+        return histo
+
+    @staticmethod
+    def vbox_from_pixels(pixels, histo):
+        rmin = 1000000
+        rmax = 0
+        gmin = 1000000
+        gmax = 0
+        bmin = 1000000
+        bmax = 0
+        for pixel in pixels:
+            rval = pixel[0] >> MMCQ.RSHIFT
+            gval = pixel[1] >> MMCQ.RSHIFT
+            bval = pixel[2] >> MMCQ.RSHIFT
+            rmin = min(rval, rmin)
+            rmax = max(rval, rmax)
+            gmin = min(gval, gmin)
+            gmax = max(gval, gmax)
+            bmin = min(bval, bmin)
+            bmax = max(bval, bmax)
+        return VBox(rmin, rmax, gmin, gmax, bmin, bmax, histo)
+
+    @staticmethod
+    def quantize(pixels, max_color):
+        """Quantize.
+
+        :param pixels: a list of pixel in the form (r, g, b)
+        :param max_color: max number of colors
+        """
+        if not pixels:
+            raise Exception('Empty pixels when quantize.')
+        if max_color < 2 or max_color > 256:
+            raise Exception('Wrong number of max colors when quantize.')
+
+        histo = MMCQ.get_histo(pixels)
+        histo_size = 1 << (3 * MMCQ.SIGBITS)
+
+        # check that we aren't below maxcolors already
+        if len(histo) <= max_color:
+            # generate the new colors from the histo and return
+            pass
+
+        # get the beginning vbox from the colors
+        vbox = MMCQ.vbox_from_colors(pixels, histo)
+        pq = PQueue()
+
+
+class VBox(object):
+    """3d color space box"""
+    def __init__(self, r1, r2, g1, g2, b1, b2, histo):
+        self.r1 = r1
+        self.r2 = r2
+        self.g1 = g1
+        self.g2 = g2
+        self.b1 = b1
+        self.b2 = b2
+        self.histo = histo
+
+    @cached_property
+    def volume(self):
+        sub_r = self.r2 - self.r1
+        sub_g = self.g2 - self.g1
+        sub_b = self.b2 - self.b1
+        return (sub_r + 1) * (sub_g + 1) * (sub_b + 1)
+
+    @property
+    def copy(self):
+        return VBox(self.r1, self.r2, self.g1, self.g2,
+                    self.b1, self.b2, self.histo)
+
+    @cached_property
+    def avg(self):
+        ntot = 0
+        mult = 1 << (8 - MMCQ.SIGBITS)
+        r_sum = 0
+        g_sum = 0
+        b_sum = 0
+        for i in range(self.r1, self.r2 + 1):
+            for j in range(self.g1, self.g2 + 1):
+                for k in range(self.b1, self.b2 + 1):
+                    histoindex = MMCQ.get_color_index(i, j, k)
+                    hval = self.histo.get(histoindex, 0)
+                    ntot += hval
+                    r_sum += hval * (i + 0.5) * mult
+                    g_sum += hval * (j + 0.5) * mult
+                    b_sum += hval * (k + 0.5) * mult
+
+        if ntot:
+            r_avg = int(r_sum / ntot)
+            g_avg = int(g_sum / ntot)
+            b_avg = int(b_sum / ntot)
+        else:
+            r_avg = int(mult * (self.r1 + self.r2 + 1) / 2)
+            g_avg = int(mult * (self.g1 + self.g2 + 1) / 2)
+            b_avg = int(mult * (self.b1 + self.b2 + 1) / 2)
+
+        return r_avg, g_avg, b_avg
+
+    def contains(self, pixel):
+        rval = pixel[0] >> MMCQ.RSHIFT
+        gval = pixel[1] >> MMCQ.RSHIFT
+        bval = pixel[2] >> MMCQ.RSHIFT
+        return all([
+            rval >= self.r1,
+            rval <= self.r2,
+            gval >= self.g1,
+            gval <= self.g2,
+            bval >= self.b1,
+            bval <= self.b2,
+        ])
+
+    @cached_property
+    def count(self):
+        npix = 0
+        for i in range(self.r1, self.r2 + 1):
+            for j in range(self.g1, self.g2 + 1):
+                for k in range(self.b1, self.b2 + 1):
+                    index = MMCQ.get_color_index(i, j, k)
+                    npix += self.histo.get(index, 0)
+        return npix
+
+
+class CMap(object):
+    """Color map"""
+    def __init__(self):
+        self.vboxes = PQueue(lambda x: x['vbox'].count * x['vbox'].volume)
+
+    @property
+    def palette(self):
+        return self.vboxes.map(lambda x: x['color'])
+
+    def push(self, vbox):
+        self.vboxes.push({
+            'vbox': vbox,
+            'color': vbox.avg,
+        })
+
+    def size(self):
+        return self.vboxes.size()
+
+    def nearest(self, color):
+        d1 = None
+        p_color = None
+        for i in range(self.vboxes.size()):
+            vbox = self.vboxes.peek(i)
+            d2 = math.sqrt(
+                math.pow(color[0] - vbox['color'][0], 2) +
+                math.pow(color[1] - vbox['color'][1], 2) +
+                math.pow(color[2] - vbox['color'][2], 2)
+            )
+            if d1 is None or d2 < d1:
+                d1 = d2
+                p_color = vbox['color']
+        return p_color
+
+    def map(self, color):
+        for i in range(self.vboxes.size()):
+            vbox = self.vboxes.peek(i)
+            if vbox['vbox'].contains(color):
+                return vbox['color']
+        return self.nearest(color)
+
+
+class PQueue(object):
+    """Simple priority queue."""
+    def __init__(self, sort_key):
+        self.sort_key = sort_key
+        self.contents = []
+        self._sorted = False
+
+    def sort(self):
+        self.contents.sort(key=self.sort_key)
+        self._sorted = True
+
+    def push(self, o):
+        self.contents.push(o)
+        self._sorted = False
+
+    def peek(self, index=None):
+        if not self._sorted:
+            self.sort()
+        if index is None:
+            index = len(self.contents) - 1
+        return self.contents[index]
+
+    def pop(self):
+        if not self._sorted:
+            self.sort()
+        return self.contents.pop()
+
+    def size(self):
+        return len(self.contents)
+
+    def map(self, f):
+        if not self._sorted:
+            self.sort()
+        return map(f, self.contents)
